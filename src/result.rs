ed1c7a84898e59ab2358cb7555b9cf98a816a688
jasongoodwin <jay.michael.goodwin@gmail.com>
2022-10-06 19:19:35 -0700

    test(result): prove we can use and print it.

--- src/result.rs
+++ src/result.rs
@@ -1,6 +1,7 @@
 use std::result::Result as StdResult;
 
 /// Result is an type alias for a Result<T, Error> to reduce type noise.
+/// This is a common pattern. It really does simplify the types in your project!
 pub type Result<T> = StdResult<T, Box<dyn std::error::Error + Send + Sync>>;
 
 /// InvalidMessage is an error.
@@ -23,3 +24,16 @@ impl std::fmt::Display for InvalidMessage {
 }
 
 impl std::error::Error for InvalidMessage {}
+
+#[cfg(test)]
+mod tests {
+    use crate::result::InvalidMessage;
+
+    #[test]
+    // demonstrates we can capture the InvalidResult as an std::Result and print.
+    // the abstraction just simplifies the types in code to make it more readable.
+    fn invalid_message_should_print_debug_msg() {
+        let msg: crate::result::Result<_> = InvalidMessage::new::<String>("these are the details".into());
+        assert_eq!(format!("{:?}", msg), "Err(InvalidMessage { details: \"these are the details\" })");
+    }
+}
